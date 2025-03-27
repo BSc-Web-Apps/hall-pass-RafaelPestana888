2ca806558b064e91a1c998acce57adab50971ab5
RafaelPestana888 <rp114063@hc.ac.je>
2025-03-27 16:29:57 +0000

    .

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,15 @@
-import { View, Text } from "react-native";
+import { Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View className="flex flex-1 py-32 bg-background">
-      <Text className="text-white text-center">Hello, world!</Text>
+      <View className="flex flex-row h-20 w-full border-2 border-cyan-400">
+        <View className="flex w-24 h-full border-2 border-pink-400"></View>
+        <View className="flex flex-1 h-full border-2 border-green-400">
+        <Text className="text-foreground">Feed the cat</Text>
+        <Text className="text-foreground">Feed the cat</Text>
+        </View>
+      </View>
     </View>
   );
 }
